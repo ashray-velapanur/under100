410b44545e6225a08d7d4478389718c5cd30a67e
Ashray <ashray.velapanur@gmail.com>
2015-01-17 17:42:07 +0530

    added endpoint to track clicking on products

---
+++ app/code/local/Beagles/Rewards/Model/Clicks.php
@@ -0,0 +1,8 @@
+<?php
+class Beagles_Rewards_Model_Clicks extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('rewards/clicks');
+    }
+}
---
+++ app/code/local/Beagles/Rewards/controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+class Beagles_Rewards_IndexController extends Mage_Core_Controller_Front_Action {        
+
+    public function indexAction() {
+
+        echo 'Hello Index!';
+
+    }
+
+    public function updateAction() {
+        $pid = intval($this->getRequest()->getParam('pid'));
+        $clicks = Mage::getModel('rewards/clicks')->load($pid);
+        if (!$clicks->getData()) {
+        	$clicks->setId($pid);
+        	$clicks->setCount(1);
+        } else {
+	        $curr_count = $clicks->getData('count');
+	        $clicks->setCount($curr_count + 1);
+        }
+ 		$clicks->save();
+    }
+
+	public function testModelAction() {
+	    $blogpost = Mage::getModel('rewards/clicks');
+        $pid = intval($this->getRequest()->getParam('pid'));
+	    $blogpost->load($pid);
+	    $data = $blogpost->getData();
+	    if (!$data) {
+	    	echo 'no data';
+	    }
+	    var_dump($data);
+	}
+}
