e113c769911a46c66a011b8d6b53bfdb03594fcf
Ashray <ashray.velapanur@gmail.com>
2015-02-13 15:45:15 +0530

    added magento core directory to repo

---
+++ app/code/core/Mage/Rating/Model/Api.php
@@ -0,0 +1,51 @@
+<?php
+class Mage_Rating_Model_Api extends Mage_Api_Model_Resource_Abstract{
+	public function _construct(){
+		$this->model= Mage::getModel("raiting/raiting");
+	}
+	
+// 	public function likeProduct($arg){
+
+// 		if(!isset($arg['productID'])) return (array("error"=>"400")); //Malformed request.
+//         if(empty($arg['productID'])) return (array("error"=>"401"));;	//Empty product id
+//         if(!isset($arg['uid'])) return (array("error"=>"400")); //Malformed request.
+//         if(empty($arg['uid'])) return (array("error"=>"402"));	//Empty uid
+//         $productID=$arg['productID'];
+//         $uid=$arg['uid'];
+//         $response=$this->helper->addLike($productID, $uid);
+// 		if($response) return array("response"=>$this->helper->countLikes($productID)); else return array(array("error"=>"500"));
+
+
+// 	}
+
+
+
+
+	public function getLikes($arg)
+    {
+    	return Mage::getModel("rating/rating")->getOptions();
+    	return 1; $this->info();
+        Mage::Log("incoming SOAP request:-->");
+        Mage::Log($arg);
+        if(!isset($arg['productID'])) return (array("error"=>"400")); //Malformed request.
+        if(empty($arg['productID'])) return (array("error"=>"401"));;	//Empty request
+        $output=array();
+        foreach ($arg['productID'] as $productID){
+        	$output[$productID]=$this->helper->countLikes($productID);
+       	}
+       	return $output;
+	}
+
+
+// 	public function getLiked($arg){
+// 		if(!isset($arg['uid'])) return array(array("error"=>"400")); //Malformed request.
+// 		if(empty($arg['uid'])) return array(array("error"=>"401"));	//Empty request
+// 		$uid=$arg['uid'];
+// 		return $this->helper->getLiked($uid);
+// 	}
+
+ 	public function info(){
+         return "*YEY* It works! .... :P";
+     }
+
+}
