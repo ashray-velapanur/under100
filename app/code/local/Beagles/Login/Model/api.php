e3f72a0600efd7ad6bf7b939d58f20344560ec55
Ashray <ashray.velapanur@gmail.com>
2015-02-20 11:28:49 +0530

    removed normal social login endpoint

--- app/code/local/Beagles/Login/Model/api.php
+++ app/code/local/Beagles/Login/Model/api.php
@@ -50,32 +50,4 @@ class Beagles_Login_Model_Api extends Mage_Api_Model_Resource_Abstract
 	    }
 	    return $response;
 	}
-
-
-    public function login($email, $firstName, $lastName)
-    {
-    	$websiteId = 1;
-        $store = Mage::app()->getStore();
-
-    	$customer = Mage::getModel("customer/customer");
-    	$customerObj = $customer->setWebsiteId($websiteId)->loadByEmail($email);
-
-        $session = Mage::getSingleton('customer/session');
-    	$response = array();
-
-    	if ($customerObj->getId()) {
-            $session->setCustomerAsLoggedIn($customer);
-    	} else {
-    		$password = Mage::helper('core')->getRandomString($length=7);
-	        $customer->setWebsiteId($websiteId)
-                 ->setStore($store)
-                 ->setFirstname($firstName)
-                 ->setLastname($lastName)
-                 ->setEmail($email)
-                 ->setPassword($password)
-                 ->save();
-    	}
-        $response = array('firstName'=>$customerObj->getFirstname(), 'lastName'=>$customerObj->getLastname(), 'customerId'=>$customerObj->getId());
-		return $response;
-    }
 }
