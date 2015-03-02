ea41bb8e5571ad8ffa31a9e9f5041609f70ebb7b
Ashray <ashray.velapanur@gmail.com>
2015-03-02 11:54:27 +0530

    updated brands endpoint

--- app/code/local/Beagles/Brands/Model/api.php
+++ app/code/local/Beagles/Brands/Model/api.php
@@ -1,13 +1,15 @@
 <?php
 class Beagles_Brands_Model_Api extends Mage_Api_Model_Resource_Abstract
 {
-	public function brands($brandName){
+	public function brands(){
+		Mage::log('... brands');
+		$baseUrl = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA);
 		$brands = Mage::getModel('brands/brands')
-					->getCollection()
-					->addFieldToFilter('name', strtolower($brandName));
+					->getCollection();
+					#->addFieldToFilter('name', strtolower($brandName));
 		$response = array();
 		foreach ($brands as $brand) {
-			$response = array('name'=>$brand->getName(), 'image_url'=>$brand->getImageUrl(), 'description'=>$brand->getDescription());
+			$response[] = array('name'=>$brand->getName(), 'image_url'=>$baseUrl.$brand->getImageUrl(), 'description'=>$brand->getDescription());
 		}
 		return $response;
 	}
