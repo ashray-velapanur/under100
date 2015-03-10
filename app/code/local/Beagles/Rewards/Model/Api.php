6533aba4d32506eeb721a3d9045fd40f226685c4
Ashray <ashray.velapanur@gmail.com>
2015-03-10 17:38:56 +0530

    completed endpoint to calculate customer score

--- app/code/local/Beagles/Rewards/Model/Api.php
+++ app/code/local/Beagles/Rewards/Model/Api.php
@@ -1,17 +1,31 @@
 <?php
 class Beagles_Rewards_Model_Api extends Mage_Api_Model_Resource_Abstract
 {
-	public function level($customerId){
-		$clicks = Mage::getModel('rewards/clicks')->getCollection()->addFieldToFilter('uid', $customerId);
-		#$comments = Mage::getModel('gaussdev_comments/comment')->getCollection()->addFieldToFilter('customer_id', $customerId);
-		$products = Mage::getModel('catalog/product')->getCollection()->addFieldToFilter('product_owner_id', $customerId);
-		$productLikes = 0;
-		$productComments = 0;
-		foreach ($products as $product) {
-			$productId = $product->getId();
-	        $productLikes += Mage::helper('GaussDev_Like')->countLikes($productId);
-	        $productComments += Mage::helper('gaussdev_comments')->commentCount($productId);
-		}
-		return array('clicks'=>count($clicks), 'likes'=>$productLikes, 'comments'=>$productComments, 'products'=>count($products));
-	}
+    public function level($customerId){
+        $clicksCount = count(Mage::getModel('rewards/clicks')->getCollection()->addFieldToFilter('uid', $customerId));
+        $products = Mage::getModel('catalog/product')->getCollection()->addFieldToFilter('product_owner_id', $customerId);
+        $productsCount = count($products);
+        $productLikesCount = 0;
+        $productCommentsCount = 0;
+        foreach ($products as $product) {
+            $productId = $product->getId();
+            $productLikesCount += Mage::helper('GaussDev_Like')->countLikes($productId);
+            $productCommentsCount += Mage::helper('gaussdev_comments')->commentCount($productId);
+        }
+        $levels = array(0.0, 10.0, 50.0);
+        $score = (0.40 * $productLikesCount) + (0.40 * $productCommentsCount) + (0.20 * $productsCount);
+        if ($score <= $levels[1]) {
+            $level = 'one';
+        } elseif ($score > $levels[1] && $score <= $levels[2]) {
+            $level = 'two';
+        } elseif ($score > $levels[2]) {
+            $level = 'three';
+        }
+        return array('level'=>$level,
+                     'score'=>$score,
+                     'clicks'=>$clicksCount,
+                     'likes'=>$productLikesCount,
+                     'comments'=>$productCommentsCount,
+                     'products'=>$productsCount);
+    }
 }
